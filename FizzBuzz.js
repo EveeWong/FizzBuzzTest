1d7b18b4bb864de7482cd70b5c4b2f0fa03f2319
Yvonne Wong <Yvonne.Wong@softwire.com>
2021-01-18 23:34:17 +0000

    Prints FizzBuzz if divisible by 3 & 5

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -6,7 +6,9 @@ function hello() {
 hello()
 
 for (let i = 1; i <= 100; i++) {
-if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz')
+    } else if (i % 3 === 0) {
     console.log('Fizz')
 } else if (i % 5 === 0) {
     console.log('Buzz')
