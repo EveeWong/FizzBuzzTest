d7eddbf2bba3e0a5888cad514cf784bc23005df1
Yvonne Wong <Yvonne.Wong@softwire.com>
2021-01-19 11:19:44 +0000

    messy code, adding Fezz function

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -5,15 +5,25 @@ function hello() {
 
 hello()
 
-for (let i =1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0) {
+for (let i =1; i <= 255; i++) {
+    if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0 && i % 13 === 0) {
+        console.log('FizzFezzBuzzBang')
+    } else if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0) { 
         console.log('FizzBuzzBang')
     } else if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz') }
-    else if (i % 3 === 0 && i % 7 === 0) {
+        console.log('FizzBuzz') 
+    } else if (i % 3 === 0 && i % 7 === 0) {
         console.log('FizzBang') 
     } else if (i % 5 === 0 && i % 7 === 0) {
         console.log('BuzzBang') 
+    } else if ( i % 13 === 0 && i % 11 === 0) {
+        console.log('FezzBong')
+    } else if ( i % 13 === 0 && i % 3 === 0) {
+        console.log('FizzFezz')
+    } else if ( i % 13 && i % 5 === 0) {
+        console.log('FezzBuzz')
+    } else if (i % 13 === 0) {
+        console.log('Fezz')
     } else if (i % 11 === 0) {
         console.log('Bong')
     } else if (i % 3 === 0) {
