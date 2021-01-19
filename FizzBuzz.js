31c6c293dd8b2e543d9a191d2609a05780bd5902
Yvonne Wong <Yvonne.Wong@softwire.com>
2021-01-19 10:36:50 +0000

    Added FizzBuzzBang

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -5,14 +5,23 @@ function hello() {
 
 hello()
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz')
+for (let i =1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0) {
+    console.log('FizzBuzzBang')
+    } else if (i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz') }
+    else if (i % 3 === 0 && i % 7 === 0) {
+    console.log('FizzBang') 
+    } else if (i % 5 === 0 && i % 7 === 0) {
+    console.log('BuzzBang') 
     } else if (i % 3 === 0) {
     console.log('Fizz')
-} else if (i % 5 === 0) {
+    } else if (i % 5 === 0) {
     console.log('Buzz')
-} else {
+    } else if ( i % 7 === 0) {
+    console.log('Bang')
+    } else {
     console.log(i)
+    }
 }
-}
+
