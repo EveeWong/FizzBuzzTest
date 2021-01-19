85aee14e0e838fc1b41884e422c677d9e5681125
Yvonne Wong <Yvonne.Wong@softwire.com>
2021-01-19 10:42:11 +0000

    Added Bong for numbers divisble by 11

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -7,21 +7,23 @@ hello()
 
 for (let i =1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0) {
-    console.log('FizzBuzzBang')
+        console.log('FizzBuzzBang')
     } else if (i % 3 === 0 && i % 5 === 0) {
-    console.log('FizzBuzz') }
+        console.log('FizzBuzz') }
     else if (i % 3 === 0 && i % 7 === 0) {
-    console.log('FizzBang') 
+        console.log('FizzBang') 
     } else if (i % 5 === 0 && i % 7 === 0) {
-    console.log('BuzzBang') 
+        console.log('BuzzBang') 
+    } else if (i % 11 === 0) {
+        console.log('Bong')
     } else if (i % 3 === 0) {
-    console.log('Fizz')
+        console.log('Fizz')
     } else if (i % 5 === 0) {
-    console.log('Buzz')
+        console.log('Buzz')
     } else if ( i % 7 === 0) {
-    console.log('Bang')
+        console.log('Bang')
     } else {
-    console.log(i)
+        console.log(i)
     }
 }
 
